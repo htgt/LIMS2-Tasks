90a0ba6364fe7e346e2ff9ba4674df14a97ffbe9
Ray Miller <rm7@sanger.ac.uk>
2012-08-01 11:42:44 +0100

    Implement CleanReportCache task

---
+++ lib/LIMS2/Task/General/CleanReportCache.pm
@@ -0,0 +1,30 @@
+package LIMS2::Task::General::CleanReportCache;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Moose;
+use LIMS2::Report;
+use namespace::autoclean;
+
+extends 'LIMS2::Task';
+
+override abstract => sub {
+    'Delete stale entries from the report cache';
+};
+
+sub execute {
+    my ( $self, $opts, $args ) = @_;
+
+    my $verbose = $self->trace || $self->debug || $self->verbose;
+
+    LIMS2::Report::clean_cache( $self->model, $verbose );
+
+    return;
+}
+
+__PACKAGE__->meta->make_immutable;
+
+1;
+
+__END__
